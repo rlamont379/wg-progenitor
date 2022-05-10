5713619e44add3f8a83778ad0bfdc4f060f82470
unknown <rlamont1@mail.csuchico.edu>
2022-05-09 21:22:42 -0700

    Deployment test 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ import * as domain from '../../auth_config.json';
 import * as clientId from '../../auth_config.json'
 
 export const environment = {
-  production: false,
+  production: true,
   auth: {
     domain,
     clientId,
-    redirectUri: window.location.origin
+    redirectUri: window.location.origin,
   },
 };
